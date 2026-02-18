1a9debe12299d97f4f9a9bc49921215dc98439d8
Nick LaSorte <37697540+nicklasorte@users.noreply.github.com>
2026-02-18 08:19:40 -0500

    Delete geodesicMidpoints.m

--- geodesicMidpoints.m
+++
@@ -1,6 +0,0 @@
-% -------------------------------------------------------------------------
-function [latMid, lonMid] = geodesicMidpoints(lat1, lon1, lat2, lon2)
-% Great-circle midpoint using Mapping Toolbox.
-[arcDeg, az] = distance(lat1, lon1, lat2, lon2);
-[latMid, lonMid] = reckon(lat1, lon1, arcDeg/2, az);
-end
